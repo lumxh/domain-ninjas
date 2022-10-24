d387cf6506e391643e2de6af6a43d2240de36bdc
Lum Xhakalija <lumi.xhakalija@gmail.com>
2022-10-24 22:57:10 +0200

    smooth intro

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$("#main").fuwatto({ duration: 2000, slide: "bottom-top" });
+
+$("#benefits-1").fuwatto({ duration: 2000 });
+
+$("#card-1").fuwatto({ duration: 2000, slide: "left-right" });
+$("#card-2").fuwatto({ duration: 2000, slide: "top-bottom" });
+$("#card-3").fuwatto({ duration: 2000, slide: "right-left" });
